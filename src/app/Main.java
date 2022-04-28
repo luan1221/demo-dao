60346423b3675d4bbf084f964c00d263cbb6697a
luan1221 <lssa@cin.ufpe.br>
2022-04-28 17:14:16 -0300

    insert

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,6 +5,7 @@ import model.dao.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
+import java.util.Date;
 import java.util.List;
 
 public class Main {
@@ -13,9 +14,9 @@ public class Main {
         SellerDAO sellerDao = DaoFactory.createSellerDao();
         //Seller seller = sellerDao.findById(3);
         //System.out.println(seller);
-        List<Seller> list = sellerDao.findAll();
-        for (Seller seller : list) {
-            System.out.println(seller);
-        }
+        Department department = new Department(2, null);
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New Id = " + newSeller.getId());
     }
 }
