4c885a8a631ff613895314cf5af62d570197eddc
luan1221 <lssa@cin.ufpe.br>
2022-04-28 16:34:39 -0300

    sellerDAO findById method

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,16 +2,13 @@ package app;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDAO;
-import model.entities.Department;
 import model.entities.Seller;
-
-import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Department obj = new Department(1, "Books");
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-        SellerDAO sellerdDao = DaoFactory.createSellerDao();
+
+        SellerDAO sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
         System.out.println(seller);
     }
 }
